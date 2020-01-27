29cc99c2c95e473b1070f44ab25c33593c81fc40
Marcos Bobadilla <marcos.bobadilla256@gmail.com>
2020-01-26 22:47:40 -0300

    Add MariaDB

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var os = require('os');
 var dns = require("dns");
 os.hostname();
 var fs = require('fs');
+
+var mysql = require('mysql');
+
 var html = fs.readFileSync('index.html');
 var indice = fs.readFileSync('indice.html');
 
@@ -12,18 +15,29 @@ const server = http.createServer((req, res) => {
   var parametro1=parametros[1];
   var parametro2=parametros[2];
   var parametro3=parametros[3];
-
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html'); 
-  console.log(parametro1.toUpperCase());
-  if(parametro1.toUpperCase()==="INDICE")
-  {
-	res.write(indice);  
-  }
-  else{
-	res.write(html);  
-  }
-  res.end();
+  var connection = mysql.createConnection({
+		host     : 'localhost',
+		user     : 'root',
+		password : 'GX9qcxth..',
+		port     : 3306,
+		database : 'base'
+  });
+  try 
+  		{
+      var consultaSQL="SELECT * FROM usuarios";
+			connection.connect();
+			connection.query(consultaSQL, function (err, result, fields) {
+          if (err) throw err;
+          res.writeHead(200, {"Content-Type": "text/json"});
+    			res.write(JSON.stringify(result));
+          res.end();
+			});
+		    connection.end();
+		}
+		catch(error) 
+		{
+			log_consola(error);
+		}
 });
 
 dns.lookupService('127.0.0.1', port, (err, hostname, service) => {  
